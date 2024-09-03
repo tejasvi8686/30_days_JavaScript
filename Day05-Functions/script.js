0ee9f68a51ac22a384cd4c5ad35496cfd11ea872
tejavi8686 <tejasviraj8686@gmail.com>
2024-09-03 23:30:41 +0530

    javascript question 1 solved

---
+++ Day05-Functions/script.js
@@ -0,0 +1,28 @@
+//Task-1 : Write a function to check if a number is  even or odd and log the result to the console.
+
+function check(num) {
+  if (num % 2 == 0) {
+    console.log("the number is even");
+  } else {
+    console.log("the number is odd");
+  }
+}
+
+check(4);
+
+//Task-2 : Write a function to calculate the square of a number and return the result.
+
+function square (a*a){
+    return a
+}
+
+
+
+//Task-3 : Write a function expression to find the maximum of two numbers and log the result to the console.
+//Task-4 : Write a function expression to concatenate two strings and return the result.
+//Task-5 : Write and arrow function to calculate the sum of two numbers and return the result.
+//Task-6 : Write an arrow function to check if a string constains specific character and return a boolean value.
+//Task-7 : Write a function that takes two parameters and return their product. Provide a default value for the second parameter.
+//Task-8 : Write a function that takes a person's name and age and returns a greeting message.Provide a default value for the age
+//Task-9 : Write a higher-order functions that takes a function and a number, and calls the function that many times.
+//Task-10 : Write a higher-order function that takes two functions and a value, applies the first function to the value, and then applies the second function to the result.
